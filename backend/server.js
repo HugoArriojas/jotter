0374fcf9e2405a6f5e6c94c18d834a2eb004d043
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-20 18:53:44 -0500

    [task] Add basic CRUD requests to file separate from server.js and verify function on Postman

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+// back end framerwork
+const express = require('express');
+// Allows us to have .env file with vars in it
+const dotenv = require('dotenv').config();
+// specify port where server is running
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/api/goals', require('./routes/goalroutes'));
+
+app.listen(port, () => console.log(`Server started on port: ${port}`));
