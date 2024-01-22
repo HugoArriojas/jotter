804207d594b28fcd29ae85390ac32c0cd97c21d3
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-21 22:51:02 -0500

    [feature] Check for user details before altering goals

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 
 const goalSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'User is requred to make goals'],
+      ref: 'User',
+    },
     text: {
       type: String,
       unique: false,
