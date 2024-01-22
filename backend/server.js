d19a86caa0bcde00a613ad6992a01aebda54b2f0
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-21 22:50:03 -0500

    [feature] Create userController for routes and define user routes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalroutes'));
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/user', require('./routes/userRoutes'));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
