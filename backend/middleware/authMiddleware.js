be45ba857f1caf96e282ba6c81ac27320510df41
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-24 22:51:40 -0500

    [feature] Create basic authSlice and add docs to mauthMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,21 +6,22 @@ const protect = asyncHandler(async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
+    // When token is sent in auth header, it starts with 'Bearer'
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      // Get token from header
+      // Get token from header by splitting into array: [Bearer, token]
       token = req.headers.authorization.split(' ')[1];
 
       // Verify token
       const decode = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Get user from the token
+      // Get user from the token but don't include password
       req.user = await User.findById(decode.id).select('-password');
 
       next();
     } catch (error) {
-      console.log(error);
+      console.log('Middleware Error:', error);
       res.status(401);
       throw new Error('Not authorized');
     }
