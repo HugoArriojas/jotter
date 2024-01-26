9b714b8202817561d70a4ae597600eb5baf7313d
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-25 22:40:33 -0500

    [feature] Add login functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
-import { FaUser } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -8,6 +13,25 @@ function Login() {
   });
 
   const { email, password } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (event) => {
     setFormData((prevState) => ({
@@ -18,13 +42,24 @@ function Login() {
 
   const onSubmit = (event) => {
     event.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
       <section className='heading'>
         <h1>
-          <FaUser /> Register
+          <FaSignInAlt /> Login
         </h1>
         <p>Please log in to add your goals</p>
       </section>
@@ -61,7 +96,7 @@ function Login() {
               type='submit'
               className='btn btn-block'
             >
-              Create Account
+              Submit
             </button>
           </div>
         </form>
