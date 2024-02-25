b7a26154a368cef369c4e57afcef819d9583623f
HugoArriojas <hugo.arriojas@hotmail.com>
2024-02-25 16:09:14 -0500

    [task] Update the error that appears with blank goal text

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -18,7 +18,7 @@ const getGoals = asyncHandler(async (req, res) => {
 const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
-    throw new Error('Please add a text field');
+    throw new Error('Please add some text to your goal');
   }
 
   const goal = await Goal.create({
