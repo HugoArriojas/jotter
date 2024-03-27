029ad510e6e70a03eb287fbf3020fb8bb1920437
HugoArriojas <hugo.arriojas@hotmail.com>
2024-03-27 18:02:12 -0400

    [task] Remove extraneous connect definition in database

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 // All mongoose methods are async as they return a promise
 const connectDB = async () => {
   try {
-    // const conn = await mongoose.connect(process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     // using colors package to style this message
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
